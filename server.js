f4540b13a2fa3ad8fdf6a132c3be470cbe5d05cf
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-12-04 11:26:48 +0530

    Image generation backend created

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const axios = require('axios')
 const bodyParser = require('body-parser')
 
 // app.use(express.json());
@@ -38,9 +39,38 @@ app.post('/ask', async (req, res) => {
         res.send(data.choices[0].message.content.trim())
     }
     catch (e) {
-        console.log(error)
+        console.log(e)
     }
 })
+
+app.post('/image', async (req, res) => {
+    const { imageQuery } = req.body
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+            model: 'dall-e-3',
+            prompt: imageQuery,
+            n: 1,
+            size: "1024x1024"
+        })
+    }
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+        const data = await response.json()
+        res.send(data)
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
